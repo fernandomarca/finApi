fa2ebf02f4be1c22d05af51790b2ffd55e045e80
Fernando Marca Magalhães <59258828+fernandomarca@users.noreply.github.com>
2021-04-11 20:27:50 -0300

    deposit feature

--- src/index.js
+++ src/index.js
@@ -1,8 +1,70 @@
 const express = require("express");
+const { v4: uuidv4 } = require("uuid");
+
+const customers = [];
 
 const app = express();
 
+//middleware
+function verifyIfExistsAccountCPF(request, response, next) {
+  const { cpf } = request.headers;
+
+  const customer = customers.find((customer) => customer.cpf === cpf);
+
+  if (!customer) {
+    return response.status(400).json({ error: "customer not found" });
+  }
+
+  request.customer = customer;
+  return next();
+}
+
 app.use(express.json());
+
+app.post("/account", (request, response) => {
+  const { cpf, name } = request.body;
+
+  const customerAlreadyExists = customers.some(
+    (customer) => customer.cpf === cpf
+  );
+
+  if (customerAlreadyExists) {
+    return response.status(400).json({ error: "Customer already exists!" });
+  }
+
+  customers.push({
+    cpf,
+    name,
+    id: uuidv4(),
+    statement: [],
+  });
+
+  return response.status(201).send();
+});
+
+//app.use(verifyIfExistsAccountCPF)
+
+app.get("/statement", verifyIfExistsAccountCPF, (request, response) => {
+  const { customer } = request;
+
+  return response.json(customer.statement);
+});
+
+app.post("/deposit", verifyIfExistsAccountCPF, (request, response) => {
+  const { description, amount } = request.body;
+
+  const { customer } = request;
+
+  const statementOperation = {
+    description,
+    amount,
+    created_at: new Date(),
+    type: "credit",
+  };
+
+  customer.statement.push(statementOperation);
+  return response.status(201).send();
+});
 
 app.listen(3333, () => {
   console.log("🚀 Servidor rodando na porta 3333");
